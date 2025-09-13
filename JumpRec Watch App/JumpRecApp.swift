5370b32bb01d9fc88bd88cfbd5fea5aa559cc8bf
Kin Yuunan <yuunan.kin@gmail.com>
2025-09-13 20:14:52 +0900

    Initial Commit

---
+++ JumpRec Watch App/JumpRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpRecApp.swift
+//  JumpRec Watch App
+//
+//  Created by Yuunan kin on 2025/09/13.
+//
+
+import SwiftUI
+
+@main
+struct JumpRec_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JumpRec/JumpRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpRecApp.swift
+//  JumpRec
+//
+//  Created by Yuunan kin on 2025/09/13.
+//
+
+import SwiftUI
+
+@main
+struct JumpRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
